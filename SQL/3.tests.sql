268458fb402ecf298cf25c4070f941a4610a738b
AdamSz00 <adamsz.form@gmail.com>
2025-09-15 21:01:36 +0000

    main

--- SQL/3.tests.sql
+++ SQL/3.tests.sql
@@ -1,7 +1,6 @@
 SELECT * FROM unique_authors;
 
-SELECT * FROM reddit_posts
-LIMIT 600;
+SELECT COUNT(post_id) FROM toxicity_results;
 
 SELECT * FROM toxicity_results;
 LIMIT 600;
@@ -11,12 +10,17 @@ DELETE FROM unique_authors;
 DELETE FROM reddit_posts;
 DELETE FROM toxicity_results;
 
-
+-- find top 10 submissions with the highest number of toxic comments
 SELECT 
-    rp.body,
-    tr.*
-FROM
-    reddit_posts as rp 
-INNER JOIN
-    toxicity_results as tr ON rp.post_id = tr.post_id
-;
+    rp.submission_id,
+    COUNT(*) AS toxic_comment_count
+FROM 
+    reddit_posts rp
+JOIN 
+    toxicity_results tr ON rp.post_id = tr.post_id
+WHERE 
+    tr.overall_toxicity > 0.5
+  AND rp.type = 'comment'
+GROUP BY rp.submission_id
+ORDER BY toxic_comment_count DESC
+LIMIT 10;
